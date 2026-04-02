21a6315657a2a6fa5207fe8b9865bf86455986ec
Matteo Smerlak <matteo.smerlak@gmail.com>
2026-04-02 22:10:54 +0200

    deploy as github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Forrofestivalfrontend/',
     plugins: [react(), tailwindcss()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
